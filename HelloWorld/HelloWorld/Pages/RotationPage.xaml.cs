0f60a36eee2d5613a15a42be8b31a8501e8793d1
Janiek <janiek.colpaert@howest.be>
2024-02-22 15:50:11 +0100

    Add data binding

---
+++ HelloWorld/HelloWorld/Pages/RotationPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RotationPage : ContentPage
+    {
+        public RotationPage()
+        {
+            InitializeComponent();
+
+            //lblText.BindingContext = sldRotation;
+            //lblText.SetBinding(Label.RotationProperty, "Value");
+        }
+
+        //private void SldRotation_ValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+            //var degrees = sldRotation.Value;
+            //lblText.Rotation = degrees;
+        //}
+    }
+}
